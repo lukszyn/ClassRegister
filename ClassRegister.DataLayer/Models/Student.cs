c1819344aa48b5b7c3b986dc6d423d0787919168
raley-time <w.tyszkiewicz@areaprojekt.pl>
2021-03-02 19:50:32 +0100

    add student model refactor

--- ClassRegister.DataLayer/Models/Student.cs
+++ ClassRegister.DataLayer/Models/Student.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace ClassRegister.DataLayer.Models
 {
-    class Class1
+    class Student
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
     }
 }
