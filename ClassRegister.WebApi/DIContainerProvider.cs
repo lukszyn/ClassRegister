c34aa7b2adfae1f72f200eecc386e1c06c94bd8f
Łukasz Szynkiewicz <szynkiewicz.luk@gmail.com>
2021-03-02 19:41:28 +0100

    DI containers added

---
+++ ClassRegister.WebApi/DIContainerProvider.cs
@@ -0,0 +1,23 @@
+﻿using ClassRegister.DataLayer;
+using System;
+using Unity;
+using Unity.Injection;
+
+namespace ClassRegister.WebApi
+{
+    public class DIContainerProvider
+    {
+        public IUnityContainer GetContainer()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<Func<IClassRegisterDbContext>>(
+                new InjectionFactory(ctx => new Func<IClassRegisterDbContext>(() => new ClassRegisterDbContext())));
+
+            return container;
+        }
+    }
+}
+
+
+
