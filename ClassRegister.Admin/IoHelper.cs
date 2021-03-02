323016731e8d088e852b9bbea160f110b0a782c4
Sylwia Ignerowicz <jagsyl2@gmail.com>
2021-03-02 19:11:33 +0100

    Added menu for Admin

---
+++ ClassRegister.Admin/IoHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassRegister.Admin
+{
+    public interface IIoHelper
+    {
+        int GetIntFromUser(string message);
+        string GetStringFromUser(string message);
+    }
+
+    public class IoHelper : IIoHelper
+    {
+        public int GetIntFromUser(string message)
+        {
+            int result;
+
+            while (!int.TryParse(GetStringFromUser(message), out result))
+            {
+                Console.WriteLine("Not an integer - try again...");
+            }
+
+            return result;
+        }
+
+        public string GetStringFromUser(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+    }
+}
--- ClassRegister.Admin/Program.cs
+++ ClassRegister.Admin/Program.cs
@@ -4,9 +4,62 @@ namespace ClassRegister.Admin
 {
     class Program
     {
+        private readonly IIoHelper _ioHelper;
+
+        //TODO: add container
+        //public Program(IIoHelper ioHelper)
+        //{
+        //    _ioHelper = ioHelper;
+        //}
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program().Run();
+        }
+
+        private void Run()
+        {
+            do
+            {
+                Console.WriteLine("Choose option:");
+                Console.WriteLine("Press 1 to Add a coach");
+                Console.WriteLine("Press 2 to Add a student");
+                Console.WriteLine("Press 3 to Add a course");
+
+                int adminChoice = _ioHelper.GetIntFromUser("Select option:");
+
+                switch (adminChoice)
+                {
+                    case 1:
+                        AddCoach();
+                        break;
+                    case 2:
+                        AddStudent();
+                        break;
+                    case 3:
+                        AddCourse();
+                        break;
+                    default:
+                        Console.WriteLine("Unknown option");
+                        break;
+                }
+
+            } while (true);
+        }
+
+        private void AddCourse()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddCoach()
+        {
+            throw new NotImplementedException();
         }
     }
 }
