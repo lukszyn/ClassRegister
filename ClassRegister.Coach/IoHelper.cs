8d4cc949b6f1fe3e3efa4ffaac9284b5be5e3e2f
Sylwia Ignerowicz <jagsyl2@gmail.com>
2021-03-04 19:14:10 +0100

    Added menu in Coach App

--- ClassRegister.Coach/IoHelper.cs
+++ ClassRegister.Coach/IoHelper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 
-namespace ClassRegister.Admin
+namespace ClassRegister.Coach
 {
     public interface IIoHelper
     {
